3d39acff88354333c1cf8093826a6179251a566e
Louis <louisandrew3@gmail.com>
2020-05-26 16:52:37 +0200

    added todos

--- src/chess/board/Board.java
+++ src/chess/board/Board.java
@@ -3,4 +3,8 @@ package chess.board;
 public class Board {
         
         private Tile[][] gameBoard;
+
+        // TODO: add function -> constructor -> create boards with pieces and tiles.
+
+        // TODO -> create kill LIsts...
 }
